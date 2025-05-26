51ac798287b6591da33c8b595ec186b07fb3a0fa
Meily Adennia <meilyadennia@students.untidar.ac.id>
2025-05-26 19:15:31 +0700

    Praktikum Mobile 8

---
+++ Praktikum Mobile 8/MainActivity.java
@@ -0,0 +1,52 @@
+ package com.example.listview;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lvItem;
+    private String[] namanegara = new String[] {
+            "Indonesia", "Malaysia", "Singapore", "Thailand", "Vietnam", "Brunei",
+            "Filipina", "Timor leste", "Italia", "Prancis", "Belanda", "Argentina",
+            "Brazil", "Monaco", "Korea Selatan", "China", "Jepang", "Swiss", "Rusia"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("List View Sederhana");
+        }
+
+        lvItem = (ListView) findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, namanegara);
+
+        lvItem.setAdapter(adapter);
+
+        lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Memilih : " + namanegara[position], Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
